d9e806a8736287fe054975c7355d7580536274be
Donald Fries <donald.darklight@gmail.com>
2020-01-28 17:46:51 -0600

    added walls to maze

--- src/MazeModel.js
+++ src/MazeModel.js
@@ -17,7 +17,7 @@ class MazeModel {
         projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
         modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
       },
-      buffers: this._initBuffers(gl, maze),
+      buffers: this._initBuffers(gl, maze)
     }
   }
 
@@ -58,7 +58,7 @@ class MazeModel {
     gl.uniformMatrix4fv(model.uniformLocations.modelViewMatrix, false, modelViewMatrix);
 
     {
-      const vertexCount = this.maze.width * this.maze.height * 6;
+      const vertexCount = buffers.vertexCount;
       const type = gl.UNSIGNED_SHORT;
       const offset = 0;
       gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
@@ -93,37 +93,108 @@ class MazeModel {
   }
 
   _initBuffers(gl, maze) {
-    const wallColor = [.7, .7, .7, 1];
-    const floorColor = [.2, .2, .2, 1.0];
-
     const positions = []
     const colors = [];
     const indices = [];
+    let offset = 0;
+
+    const addSquareXY = (left, right, top, bottom, base, color) => {
+      positions.push
+        (
+          left, top, base,
+          right, top, base,
+          right, bottom, base,
+          left, bottom, base
+        );
+      indices.push
+        (
+          offset + 0, offset + 1, offset + 2, offset + 0, offset + 2, offset + 3
+        );
+      offset += 4;
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          colors.push(color[j]);
+        }
+      }
+    }
+
+    const addSquareXZ = (left, right, top, bottom, base, color) => {
+      positions.push
+        (
+          left, base, top,
+          right, base, top,
+          right, base, bottom,
+          left, base, bottom
+        );
+      indices.push
+        (
+          offset + 0, offset + 1, offset + 2, offset + 0, offset + 2, offset + 3
+        );
+      offset += 4;
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          colors.push(color[j]);
+        }
+      }
+    }
+
+    const addSquareYZ = (left, right, top, bottom, base, color) => {
+      positions.push
+        (
+          base, left, top,
+          base, right, top,
+          base, right, bottom,
+          base, left, bottom
+        );
+      indices.push
+        (
+          offset + 0, offset + 1, offset + 2, offset + 0, offset + 2, offset + 3
+        );
+      offset += 4;
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          colors.push(color[j]);
+        }
+      }
+    }
+
+    const floorColor = [.2, .2, .2, 1.0];
+    const ceilingColor = [.7, .7, .7, 1];
+    const wallColor_Top = [.5, 0, .5, 1];
+    const wallColor_Bottom = [0, .5, 0, 1];
+    const wallColor_Left = [.5, 0, 0, 1];
+    const wallColor_Right = [0, .0, .5, 1];
+
     const ofs_x = -maze.width / 2;
     const ofs_y = -maze.height / 2;
     for (let y = 0; y < maze.height; y++) {
       for (let x = 0; x < maze.width; x++) {
-        let base;
-        let color;
-        if (maze.data[y][x] === 0) {
-          base = 0.0;
-          color = floorColor;
-        } else {
-          base = 1.0;
-          color = wallColor;
-        }
         const left = x + ofs_x;
         const right = left + 1;
         const top = y + ofs_y;
         const bottom = top + 1;
-        positions.push(left, top, base, right, top, base, right, bottom, base, left, bottom, base);
-        for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-            colors.push(color[j]);
+
+        if (maze.data[y][x] === 0) {
+          addSquareXY(left, right, top, bottom, 0.0, floorColor);
+        } else {
+          addSquareXY(left, right, top, bottom, 1.0, ceilingColor);
+
+          if (y === 0 || maze.data[y - 1][x] === 0) {
+            addSquareXZ(left, right, 0.0, 1.0, top, wallColor_Top);
+          }
+
+          if (x === 0 || maze.data[y][x - 1] === 0) {
+            addSquareYZ(top, bottom, 0.0, 1.0, left, wallColor_Left);
+          }
+
+          if (y === maze.height - 1 || maze.data[y + 1][x] === 0) {
+            addSquareXZ(left, right, 0.0, 1.0, bottom, wallColor_Bottom);
+          }
+
+          if (x === maze.width - 1 || maze.data[y][x + 1] === 0) {
+            addSquareYZ(top, bottom, 0.0, 1.0, right, wallColor_Right);
           }
         }
-        const offset = (y * maze.height + x) * 4;
-        indices.push(offset + 0, offset + 1, offset + 2, offset + 0, offset + 2, offset + 3);
       }
     }
 
@@ -139,7 +210,7 @@ class MazeModel {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
-    return { position: positionBuffer, color: colorBuffer, indices: indexBuffer };
+    return { position: positionBuffer, color: colorBuffer, indices: indexBuffer, vertexCount: indices.length };
   }
 }
 
