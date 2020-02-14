c35d58ff2a2733ba87aa73789f9f8e47fe73b067
Donald Fries <donald.darklight@gmail.com>
2020-02-14 09:00:17 -0600

    added click handler for message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Message = ({ message }) => {
+const Message = ({ message, onClick }) => {
   if (!message) {
     return null;
   }
   return (
-    <div id="message">
+    <div id="message" onClick={onClick}>
       {message.map((text, index) => (<div key={index}>{text}</div>))}
     </div>
   );
