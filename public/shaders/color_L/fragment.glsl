fb857acda7efdb88c6c6e0f2700afd7a717fec37
Donald Fries <donald.darklight@gmail.com>
2020-02-16 18:32:47 -0600

    added specular light to color_L and texture_LD

--- public/shaders/color_L/fragment.glsl
+++ public/shaders/color_L/fragment.glsl
@@ -13,6 +13,7 @@ uniform struct {
 } uPointLight;
 
 uniform int uLightingModel;
+uniform vec3 uCameraPos;
 
 varying vec3 vVertexPosition;
 varying vec3 vVertexNormal;
@@ -28,10 +29,17 @@ void main(void) {
   }
   else if (uLightingModel == 2) {
     float directional = max(dot(vVertexNormal, uDirectionalLight.direction), 0.0);
+
     vec3 surfaceToLight = uPointLight.position - vVertexPosition;
     float bright = uPointLight.brightness * max(dot(vVertexNormal, normalize(surfaceToLight)), 0.0) / (length(surfaceToLight) * length(surfaceToLight));
     bright = clamp(bright, 0.0, 1.0);
-    vec3 lighting = uAmbientLight + (uDirectionalLight.color * directional) + (uPointLight.color * bright);
+
+    vec3 viewDir = normalize(uCameraPos - vVertexPosition);
+    vec3 reflectDir = reflect(-normalize(surfaceToLight), vVertexNormal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 5.0);
+
+    vec3 lighting = uAmbientLight + (uDirectionalLight.color * directional) + (uPointLight.color * bright) + spec;
+
     gl_FragColor = vec4(vColor * lighting, 1.0);
   }
 }
--- public/shaders/texture_LD/fragment.glsl
+++ public/shaders/texture_LD/fragment.glsl
@@ -2,7 +2,8 @@ precision mediump float;
 precision lowp int;
 
 uniform sampler2D uSamplerDiffuse;
-uniform vec3 uAmbientLight;     
+
+uniform vec3 uAmbientLight;
 uniform struct {
   vec3 direction;
   vec3 color;
@@ -12,7 +13,9 @@ uniform struct {
   vec3 color;
   float brightness;
 } uPointLight;
+
 uniform int uLightingModel;
+uniform vec3 uCameraPos;
 
 varying vec3 vVertexPosition;
 varying vec3 vVertexNormal;
@@ -20,20 +23,26 @@ varying vec2 vTextureCoord;
 varying vec3 vLighting;
 
 void main(void) {
+  vec4 texelColor = texture2D(uSamplerDiffuse, vTextureCoord);
   if (uLightingModel == 0) {
-    gl_FragColor = texture2D(uSamplerDiffuse, vTextureCoord);
+    gl_FragColor = texelColor;
   }
   else if (uLightingModel == 1) {
-    vec4 texelColor = texture2D(uSamplerDiffuse, vTextureCoord);
     gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
   }
   else if (uLightingModel == 2) {
     float directional = max(dot(vVertexNormal, uDirectionalLight.direction), 0.0);
+
     vec3 surfaceToLight = uPointLight.position - vVertexPosition;
     float bright = uPointLight.brightness * max(dot(vVertexNormal, normalize(surfaceToLight)), 0.0) / (length(surfaceToLight) * length(surfaceToLight));
     bright = clamp(bright, 0.0, 1.0);
-    vec3 lighting = uAmbientLight + (uDirectionalLight.color * directional) + (uPointLight.color * bright);
-    vec4 texelColor = texture2D(uSamplerDiffuse, vTextureCoord);
+
+    vec3 viewDir = normalize(uCameraPos - vVertexPosition);
+    vec3 reflectDir = reflect(-normalize(surfaceToLight), vVertexNormal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 5.0);
+
+    vec3 lighting = uAmbientLight + (uDirectionalLight.color * directional) + (uPointLight.color * bright) + spec;
+
     gl_FragColor = vec4(texelColor.rgb * lighting, texelColor.a);
   }
 }
